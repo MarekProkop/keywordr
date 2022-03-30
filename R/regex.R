2696e62d598a262b7a3b84a7eb90e998f1500f6d
Marek Prokop <mprokop@prokopsw.cz>
2022-03-30 22:06:08 +0200

    kwr_build_regex

--- R/regex.R
+++ R/regex.R
@@ -1,3 +1,39 @@
+#' Creates regular expression from a single string
+#'
+#' The function does the following two things:
+#'
+#' 1. Accented letters in a string are replaced with a character class
+#' containing both the accented letter and the same letter without accent.
+#'
+#' 2. If `and` argument is provided, the reulting regex will match both strings
+#' in any order.
+#'
+#' @param x Input string.
+#' @param and Optional string that should be matched as well.
+#'
+#' @return Regular expression.
+#' @export
+#'
+#' @examples
+#'
+#' kwr_build_regex("vajíčko")
+#'
+#' # "vaj[íi][čc]ko"
+#'
+#' kwr_build_regex(x = "abc", and = "xyz")
+#'
+#' # "xyz.+abc|abc.+xyz"
+kwr_build_regex <- function(x, and = NULL) {
+  x <- replace_accents(x)
+  if (is.null(and)) {
+    return(x)
+  } else {
+    and <- replace_accents(and)
+    return(paste0(and, ".+", x, "|", x, ".+", and))
+  }
+}
+
+
 #' Explore a regex pattern applied on queries
 #'
 #' @param kwr A kwresearch object.
@@ -74,4 +110,21 @@ kwr_test_regex <- function(kwr, pattern, and = NULL, except = NULL, stopwords = 
     around = around,
     around_ngrams = around_ngrams
   )
+}
+
+
+# Private functions -------------------------------------------------------
+
+
+replace_accents <- function(x) {
+  char_vec <- stringr::str_split(x, "")[[1]]
+  char_vec |> purrr::map_chr(function(x) {
+    ascii = stringi::stri_trans_general(x, "Latin-ASCII")
+    if (ascii != x) {
+      stringr::str_c("[", x, ascii, "]")
+    } else {
+      x
+    }
+  }) |>
+    stringr::str_c(collapse = "")
 }
--- tests/testthat/test-regex.R
+++ tests/testthat/test-regex.R
@@ -1,3 +1,17 @@
+test_that("kwr_build_regex() replaces accents", {
+  expect_equal(
+    object = kwr_build_regex("vajíčko"),
+    expected = "vaj[íi][čc]ko"
+  )
+})
+
+test_that("kwr_build_regex() handles the and argument correctly", {
+  expect_equal(
+    object = kwr_build_regex("prší", "krásně"),
+    expected = "kr[áa]sn[ěe].+pr[šs][íi]|pr[šs][íi].+kr[áa]sn[ěe]"
+  )
+})
+
 test_that("kwr_test_regex() works", {
   input_df <- data.frame(
     query = c("aaa bbb ccc", "aaa bbb", "bbb ccc"),
